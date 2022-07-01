0ed62879470f55b676c81c7fe3de0c3509c214e4
amrendra-rathore <amrendra-rathore@successive.tech>
2022-07-01 14:40:52 +0530

    Day eighth #39522

--- src/Server.ts
+++ src/Server.ts
@@ -1,13 +1,23 @@
 import * as express from 'express';
+// import bodyParser from 'body-parser';
+import { default as notFound } from './libs/routes/notFoundRoute';
+import { default as errorHandler } from './libs/routes/errorHandler';
 
 export default class Server {
   public app;
   private config;
+
   constructor(config){
     this.config = config;
     this.app = express();
   }
+
+  public initBodyParser() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
   public bootstrap(){
+    this.initBodyParser();
     this.setupRoutes();
     return this;
   }
@@ -15,6 +25,12 @@ export default class Server {
     this.app.get("/health-check", function(req , res)  {
       res.send('I am OK');
     });
+
+    this.app.use("/error", (req, res) => {
+      throw new Error();
+    });
+    this.app.use(notFound);
+    this.app.use(errorHandler);
   }
   public run()  {
     this.app.listen(this.config.port, () => {
